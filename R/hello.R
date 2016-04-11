fc675b593c969e0159475f8c8432a28ccf80eac8
emanuelserbanoiu <contact@emanuelserbanoiu.com>
2016-04-11 19:20:16 +0300

    2nd TRY

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' Test function
+#' Hello
+#' @param none
+#' @return a string
+#' @export
+
+square <- function(x) return(x^2)
+
+#' new function here
+#' @param primeste ceva fain
+#' @return returneaza ceva nice
+#' @export
+
+
+triple <- function(x) return(x^3)

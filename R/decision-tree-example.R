2c6df16f3b0d114d12a2457f623aacf8d69da093
emanuelserbanoiu <contact@emanuelserbanoiu.com>
2016-04-11 20:58:17 +0300

    RPART test tree

---
+++ R/decision-tree-example.R
@@ -0,0 +1,15 @@
+#' Decision tree example
+#' Trying out rpart
+#' @export
+
+require(rpart)
+require(rpart.plot)
+iris  <- read.csv(url("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"), header = FALSE)
+s <- sample(150, 50)
+iris_train <- iris[s,]
+iris_test <- iris[-s,]
+dtm <- rpart(V5~., iris_train, method = "class")
+dtm
+rpart.plot(dtm, type = 4, extra = 101)
+p<-predict(dtm, iris_test, type="class")
+table(iris_test[,5], p)
